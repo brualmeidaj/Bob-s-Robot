07802640a9cec8eb60761373e289d3554285713f
Bruna Almeida <contatobrunaalmeidaj13@gmail.com>
2022-08-22 16:22:17 -0300

    plugins

--- src/api.py
+++ src/api.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def welcome(): 
     html_string = "n/a"
     path = "./src/tests/bobs.robot"
-    vars = (f"--variable website_url:{request.json['website_url']}")
+    vars = (f"--variable website_url:{request.json['website_url']} --variable website_nome:{request.json['website_nome']}")
     os.system(f"python -m robot {vars} -d ./logs {path}")
     with open("./logs/log.html", "r", encoding="utf8") as f:
         html_string = f.read()
